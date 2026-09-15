817bf2796655c3bfead1b2038f5775a51e46d655
S RAMANADHA REDDY <ramnathreddy838@gmail.com>
2026-09-15 17:58:50 +0530

    Time: 41 ms (75.16%), Space: 30.9 MB (7.6%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -6,7 +6,7 @@ class Solution:
         if w>=r:
             count+=1
         for i in range(k,len(arr)):
-            w+=arr[i]-arr[i-k] 
+            w=w+arr[i]-arr[i-k] 
             if w>=r:
                 count+=1
         return count
