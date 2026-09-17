2f1d40814f4de5670dc07824910881f2392bff5d
S RAMANADHA REDDY <ramnathreddy838@gmail.com>
2026-09-17 22:00:30 +0530

    Time: 57 ms (63.44%), Space: 22.3 MB (6.84%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,18 +3,13 @@ class Solution:
         l = 0
         zero_c = 0
         max_len = 0
-
         for i in range(len(nums)):
-
             if nums[i] == 0:
                 zero_c += 1
-
             while zero_c > k:
                 if nums[l] == 0:
                     zero_c -= 1
                 l += 1
-
             if i-l+1 > max_len:
                 max_len = i-l+1
-
         return max_len
